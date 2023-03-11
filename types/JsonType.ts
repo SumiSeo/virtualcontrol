1e636206c2ca5b4a3bdc115902f762081b4a0819
Sumi <equepem@gmail.com>
2023-03-11 23:57:28 +0100

    chore(types): refactor type name and code structure

---
+++ types/JsonType.ts
@@ -0,0 +1,9 @@
+interface IjsonArgs {
+  type: string;
+  name: string;
+  order: string;
+  id: string;
+  "sub-type": any;
+}
+
+export default IjsonArgs;
--- virtualcontrol/components/Hospital.tsx
+++ virtualcontrol/components/Hospital.tsx
@@ -1,19 +1,15 @@
 import React from "react";
 import HospitalData from "../../hospitals.json";
 
-interface jsonArgs {
-  type: string;
-  name: string;
-  order: string;
-  id: string;
-  "sub-type": any;
-}
+//types
+import IjsonArgs from "../../types/JsonType";
+
 const Hospital = (): JSX.Element => {
   const onClick = () => {
     console.log("clicked");
   };
 
-  const generateTabs = (data: jsonArgs): JSX.Element => {
+  const generateTabs = (data: IjsonArgs): JSX.Element => {
     const currentEntityType = data.type;
     const currentEntityName = data.name;
     const currentEntitySubType = data["sub-type"];
@@ -25,7 +21,7 @@ const Hospital = (): JSX.Element => {
         <div>
           {Array.isArray(data) ? (
             data.map((el: any) => (
-              <div className={el.type} key={el.firstname}>
+              <div onClick={onClick} className={el.type} key={el.firstname}>
                 <h1>{el.firstname}</h1>
               </div>
             ))

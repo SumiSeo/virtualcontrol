42401bd0987618381981d809e2c9e2da74625faa
Sumi <equepem@gmail.com>
2023-03-11 13:31:01 +0100

    feat(css): generate css based on json obj

--- virtualcontrol/components/Hospital.tsx
+++ virtualcontrol/components/Hospital.tsx
@@ -5,9 +5,11 @@ const Hospital = (): JSX.Element => {
   const createHospitalStructure = (): JSX.Element => {
     console.log("hospital", HospitalData);
     const hospital = HospitalData.type;
+    console.log("hospital type", hospital);
     const hospitalName = HospitalData.name;
 
     const contents = HospitalData["sub-type"].type;
+    console.log("contents", contents);
     const contentsName = HospitalData["sub-type"].name;
 
     //loop over json element is better than do hard-coding
@@ -17,7 +19,12 @@ const Hospital = (): JSX.Element => {
     //give the classname
     //add css
 
-    return <div></div>;
+    return (
+      <div>
+        <div className={hospital}>{HospitalData.name}</div>
+        <div className={contents}>{contentsName}</div>
+      </div>
+    );
   };
 
   return <div>{createHospitalStructure()}</div>;

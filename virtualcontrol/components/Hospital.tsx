5a9ce026a030d8060d19ad568081bef4f34367d8
Sumi <equepem@gmail.com>
2023-03-12 13:31:23 +0100

    feat(hidden): display only clicked children element using event target

--- virtualcontrol/components/Hospital.tsx
+++ virtualcontrol/components/Hospital.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 import HospitalData from "../../hospitals.json";
 
 //types
 import IjsonArgs from "../../types/JsonType";
 
 const Hospital = (): JSX.Element => {
-  const onClick = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  const onClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    console.log(e.target);
     console.log("clicked");
+    if (e.target instanceof Element) {
+      console.log(e.target.nextElementSibling);
+      const nextElements = e?.target?.nextElementSibling?.children;
+    }
   };
 
   const generateTabs = (data: IjsonArgs): JSX.Element => {
@@ -15,25 +22,27 @@ const Hospital = (): JSX.Element => {
     const currentEntitySubType = data["sub-type"];
     return (
       <>
-        <div onClick={onClick} className={currentEntityType}>
-          {currentEntityName}
-        </div>
-        <div>
-          {Array.isArray(data) ? (
-            data.map((el: any) => (
-              <div onClick={onClick} className={el.type} key={el.firstname}>
-                <h1>{el.firstname}</h1>
+        <div className="hidden">
+          <div onClick={(e) => onClick(e)} className={currentEntityType}>
+            {currentEntityName}
+          </div>
+          <div>
+            {Array.isArray(data) ? (
+              data.map((el: any) => (
+                <div onClick={onClick} className={el.type} key={el.firstname}>
+                  {el.firstname}
+                </div>
+              ))
+            ) : (
+              <div>
+                {currentEntitySubType !== null ? (
+                  generateTabs(currentEntitySubType)
+                ) : (
+                  <div></div>
+                )}
               </div>
-            ))
-          ) : (
-            <div>
-              {currentEntitySubType !== null ? (
-                generateTabs(currentEntitySubType)
-              ) : (
-                <div></div>
-              )}
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </>
     );
